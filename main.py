9751b2f67d53d126f38a35201efa76a1f02e74ab
Ben George <ben@exaspace.org>
2022-03-13 14:06:24 +0000

    Add PG JSON

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ import postgres_scenarios
 import redis_scenarios
 from aerospike_scenarios import AerospikeProductStoreScenario
 from mongo_scenarios import MongoProductStoreScenario
-from postgres_scenarios import PostgresProductStoreScenario
+from postgres_scenarios import PostgresProductStoreScenario, PostgresProductStoreScenarioJson
 from redis_scenarios import RedisProductStoreScenarioUsingHashes
 
 if __name__ == "__main__":
 
     sizes = [(200, 200), (1_000, 2_000), (10_000, 20_000)]
 
-    size_choice = 0
+    size_choice = 1
     run = [
         'aerospike',
         'cockroach',
-        'redis',
-        'postgres',
+        'cockroach_json',
         'mongo',
+        'postgres',
+        'postgres_json',
+        'redis',
+        'yugabyte',
+        'yugabyte_json',
     ]
 
     np = sizes[size_choice][0]
@@ -29,12 +33,24 @@ if __name__ == "__main__":
     if 'cockroach' in run:
         PostgresProductStoreScenario(postgres_scenarios.LOCALHOST_COCKROACH, num_products=np, num_queries=nq).execute()
 
+    if 'cockroach_json' in run:
+        PostgresProductStoreScenarioJson(postgres_scenarios.LOCALHOST_COCKROACH, num_products=np, num_queries=nq).execute()
+
     if 'mongo' in run:
         MongoProductStoreScenario(mongo_scenarios.LOCALHOST, num_products=np, num_queries=nq).execute()
 
     if 'postgres' in run:
         PostgresProductStoreScenario(postgres_scenarios.LOCALHOST, num_products=np, num_queries=nq).execute()
 
+    if 'postgres_json' in run:
+        PostgresProductStoreScenarioJson(postgres_scenarios.LOCALHOST, num_products=np, num_queries=nq).execute()
+
     if 'redis' in run:
         RedisProductStoreScenarioUsingHashes(redis_scenarios.LOCALHOST, num_products=np, num_queries=nq).execute()
+
+    if 'yugabyte' in run:
+        PostgresProductStoreScenario(postgres_scenarios.LOCALHOST_YUGABYTE, num_products=np, num_queries=nq).execute()
+
+    if 'yugabyte_json' in run:
+        PostgresProductStoreScenarioJson(postgres_scenarios.LOCALHOST_YUGABYTE, num_products=np, num_queries=nq).execute()
 
--- postgres_scenarios.py
+++ postgres_scenarios.py
@@ -1,3 +1,5 @@
+import json
+
 import psycopg2
 from psycopg2.extras import execute_values
 from scenarios import ProductStoreScenario
@@ -15,6 +17,14 @@ LOCALHOST_COCKROACH = {
     'user': 'root',
     'host': 'localhost',
     'port': '26257',
+    'password': '',
+}
+
+LOCALHOST_YUGABYTE = {
+    'dbname': 'postgres',
+    'user': 'postgres',
+    'host': 'localhost',
+    'port': '5433',
     'password': '',
 }
 
@@ -44,7 +54,7 @@ class PostgresProductStoreScenario:
     VALUES %s
     """
 
-    INSERT_SQL_1 = """
+    INSERT_SQL_NAIVE = """
     INSERT INTO products (product_id, created_ts, title, description, url, price) 
     VALUES (%(product_id)s, %(created_ts)s, %(title)s, %(description)s, %(url)s, %(price)s)
     """
@@ -77,7 +87,7 @@ class PostgresProductStoreScenario:
         # WARNING: this will be far slower than using execute_values()
         curs = self.conn.cursor()
         for i, product in enumerate(products):
-            curs.execute(self.INSERT_SQL_1, product)
+            curs.execute(self.INSERT_SQL_NAIVE, product)
             if i % 1000 == 0:
                 self.conn.commit()
         self.conn.commit()
@@ -91,4 +101,59 @@ class PostgresProductStoreScenario:
     def get_product_desc(self, product_id):
         with self.conn.cursor() as curs:
             curs.execute('SELECT description FROM products WHERE product_id = %s', (product_id,))
+            return curs.fetchone()[0]
+
+
+class PostgresProductStoreScenarioJson:
+    DROP_SQL = """
+        DROP TABLE IF EXISTS products_json;"""
+
+    CREATE_SQL = """
+        CREATE TABLE products_json (
+            doc JSONB NOT NULL 
+        )
+        """
+
+    CREATE_INDEX_SQL = """
+        CREATE INDEX products_json_idx ON products_json USING GIN(doc)
+        """
+
+    INSERT_SQL = """
+    INSERT INTO products_json (doc) 
+    VALUES %s
+    """
+
+    def __init__(self, config, num_products, num_queries):
+        self.conn = new_postgres_client(config)
+        self.scenario = ProductStoreScenario(num_products, num_queries)
+
+    def _clean_create(self):
+        with self.conn.cursor() as curs:
+            curs.execute(self.DROP_SQL)
+            self.conn.commit()
+            curs.execute(self.CREATE_SQL)
+            self.conn.commit()
+            curs.execute(self.CREATE_INDEX_SQL)
+            self.conn.commit()
+
+    def execute(self):
+        self._clean_create()
+        self.scenario.execute(self)
+
+    def load_products(self, products):
+        with self.conn.cursor() as curs:
+            seq = [(json.dumps(p),) for p in products]
+            execute_values(curs, self.INSERT_SQL, seq)
+            self.conn.commit()
+
+    def get_product_title(self, product_id):
+        with self.conn.cursor() as curs:
+            curs.execute('SELECT doc->>\'title\' FROM products_json WHERE doc->>\'product_id\' = %s',
+                         (product_id,))
+            return curs.fetchone()[0]
+
+    def get_product_desc(self, product_id):
+        with self.conn.cursor() as curs:
+            curs.execute('SELECT doc->>\'description\' FROM products_json WHERE doc->>\'product_id\' = %s',
+                         (product_id,))
             return curs.fetchone()[0]
